14fee949c79637d0cc0e88fb31834786a6f7acdc
cosmus123 <cosminmus@gmail.com>
2019-06-05 01:54:47 +0300

    Edited config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8012/code_three/';
+$config['base_url'] = 'https://wizpet.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => $url["eu-cdbr-west-02.cleardb.net"],
+	'username' => $url["be144a297faeb3"],
+	'password' => $url["d86c8ff1"],
+	'database' => substr($url["path"], 1),
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
